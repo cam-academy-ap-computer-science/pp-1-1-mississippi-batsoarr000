0ce6a80fb06de23de979a6a84fdc0d64dca5b287
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-13 07:47:44 -0700

    miss

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -2,7 +2,14 @@
 public class Mississippi {
 
 	public static void main(String[] args) {
-		System.out.println("Mississippi");
+		System.out.println("M         M   IIIIIIIIIIIII     SSSSSSSSS      SSSSSSSSSSS      IIIIIIIIIII                     ");
+		System.out.println("Mm       mM        III          SS             SS                   III                   ");
+		System.out.println("Mmm     mmM        III          SS             SS                   III               ");
+		System.out.println("MmmmmmmmmmM        III          SSSSSSSSS      SSSSSSSSSSS          III                 ");
+		System.out.println("M   MM    M        III                 SS               SS          III                 ");
+		System.out.println("M         M        III                 SS               SS          III                   ");
+		System.out.println("M         M        III          SSSSSSSSS      SSSSSSSSSSS     IIIIIIIIIII                        ");
+		System.out.println("M         M   IIIIIIIIIIIII                                                         ");
 
 	}
 
