27b9c4f8c53996b0779364517e83e5cb1ce88c53
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-17 07:47:57 -0700

    fin

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -2,19 +2,19 @@
 public class Mississippi {
 
 	public static void main(String[] args) {
-		method1();
-		method2();
-		method3();
-		method3();
-		method2();
-		method3();
-		method3();
-		method2();
-		method4();
-		method4();
-		method2();
+		m();
+		i();
+		s();
+		s();
+		i();
+		s();
+		s();
+		i();
+		p();
+		p();
+		i();
 	}
-	public static void method1() {
+	public static void m() {
 	System.out.println("M         M");
 	System.out.println("Mm       mM");
 	System.out.println("Mmm     mmM");
@@ -24,7 +24,7 @@ public class Mississippi {
 	System.out.println("M         M");
 	System.out.println("M         M");
 	}
-	public static void method2() {
+	public static void i() {
 		System.out.println("IIIIIIIIIIIII                 ");
 		System.out.println("    III                    ");
 		System.out.println("    III                      ");
@@ -34,7 +34,7 @@ public class Mississippi {
 		System.out.println("    III                        ");
 		System.out.println("IIIIIIIIIIIII             ");
 	}
-	public static void method3() {
+	public static void s() {
 		System.out.println("SSSSSSSSS                        ");
 		System.out.println("SS                            ");
 		System.out.println("SS                        ");
@@ -43,7 +43,7 @@ public class Mississippi {
 		System.out.println("       SS                ");
 		System.out.println("SSSSSSSSS                    ");
 	}
-	public static void method4() {
+	public static void p() {
 		System.out.println(" PPPPPPPPPP       ");
 		System.out.println(" PP      PP ");
 		System.out.println(" PP      PP      ");
